aa4b47d17793583dbe4735db240bfa8f6228a002
richie <richie.kwon1@gmail.com>
2022-05-18 21:03:43 +0100

    practice - register page

---
+++ BlueProject/Controllers/LController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BlueProject.Models;
+using MySqlConnector;
+
+namespace BlueProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Login()
+        {
+            return View();
+        }
+        
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
