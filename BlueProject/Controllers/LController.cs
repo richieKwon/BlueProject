1fb7bccfb0ccd48080436b2fa9b3e6d8f5a0eb4a
richie <richie.kwon1@gmail.com>
2022-05-19 21:50:33 +0100

    login debugging

--- BlueProject/Controllers/LController.cs
+++ BlueProject/Controllers/LController.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BlueProject.Models;
@@ -18,14 +19,39 @@ namespace BlueProject.Controllers
             return Redirect("/Login/Login");
         }
         
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
         
-        public IActionResult Register()
+        [HttpPost]
+        [Route("login/login")]
+        public IActionResult LoginProc([FromForm] UserModel input)
         {
+            return Json(new {});
+        }
+        
+        public IActionResult Register(string msg)
+        {
+            ViewData["msg"] = msg;
             return View();
         }
+        
+        [HttpPost]
+        [Route("login/register")]
+        public IActionResult RegisterProc([FromForm] UserModel input)
+        {
+            try
+            {
+                input.Register();
+                return Redirect("/Login/Login");
+            }
+            catch (Exception e)
+            {
+                return Redirect($"Login/Register?msg={HttpUtility.UrlEncode(e.Message)}");
+            }
+            
+        }
     }
-}
+}    
--- BlueProject/Models/UserModel.cs
+++ BlueProject/Models/UserModel.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using MySqlConnector;
 
 namespace BlueProject.Models
 {
@@ -8,5 +9,32 @@ namespace BlueProject.Models
         public string  User_name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+
+        public string ConvertPassword()
+        {
+            var sha = new System.Security.Cryptography.HMACSHA512();
+            sha.Key = System.Text.Encoding.UTF8.GetBytes(this.Password.Length.ToString());
+            var hash=sha.ComputeHash(System.Text.Encoding.UTF8.GetBytes(this.Password));
+            return System.Convert.ToBase64String(hash);
+        }
+
+        internal int Register()
+        {
+            string sql = @"
+                  INSERT INTO t_user(
+                                     user_seq,
+                                     user_name,
+                                     email,
+                                     password,
+                                     
+                  ) 
+                    values (1, @user_name, @email, @password) 
+                ";
+            using (var conn = new MySqlConnection("Server=127.0.0.1;Database=myweb;Uid=root;Pwd=dookie91Sql!;"))
+            {
+                conn.Open();
+                return Dapper.SqlMapper.Execute(conn, sql, this);
+            }
+        }
     }
 }
