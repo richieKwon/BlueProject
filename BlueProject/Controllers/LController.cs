2869373459f5bfac13217e07d0c99043f9842d1d
richie <richie.kwon1@gmail.com>
2022-05-19 19:36:32 +0100

    controller for login

--- BlueProject/Controllers/LController.cs
+++ BlueProject/Controllers/LController.cs
@@ -15,7 +15,7 @@ namespace BlueProject.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return Redirect("/Login/Login");
         }
         
         public IActionResult Login()
---
+++ BlueProject/Models/UserModel.cs
@@ -0,0 +1,12 @@
+using System.Globalization;
+
+namespace BlueProject.Models
+{
+    public class UserModel
+    {
+        public string User_seq { get; set; }
+        public string  User_name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
