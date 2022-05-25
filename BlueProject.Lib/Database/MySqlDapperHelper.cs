464a20c423f7c20b05ffc75ce03d7ebeac36e97c
richie <richie.kwon1@gmail.com>
2022-05-25 20:37:12 +0100

    refactoring db

---
+++ BlueProject.Lib/Database/MySqlDapperHelper.cs
@@ -0,0 +1,15 @@
+using MySqlConnector;
+
+namespace BlueProject.Lib.Database
+{
+    public class MySqlDapperHelper
+    {
+        private MySqlConnection _conn;
+        using (var conn = new MySqlConnection())
+        {
+            
+        }
+    }
+
+   
+}
