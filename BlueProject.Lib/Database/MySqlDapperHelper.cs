75db91471639934323ef6bed04aa64d33eed1b34
richie <richie.kwon1@gmail.com>
2022-06-06 21:16:54 +0100

    execute funtion

--- BlueProject.Lib/Database/MySqlDapperHelper.cs
+++ BlueProject.Lib/Database/MySqlDapperHelper.cs
@@ -9,12 +9,18 @@ namespace BlueProject.Lib.Database
         private MySqlConnection _conn;
         public MySqlDapperHelper()
         {
-            _conn = conn = new MySqlConnection("Server=127.0.0.1;Database=myweb;Uid=root;Pwd=dookie91Sql!;");
+            _conn = new MySqlConnection("Server=127.0.0.1;Database=myweb;Uid=root;Pwd=dookie91Sql!;");
         }
 
-        public List<T> GetQuery<T>(string sql, object praram)
+        public List<T> Query<T>(string sql, object param)
         {
             return Dapper.SqlMapper.Query<T>(_conn, sql, param).ToList();
+        }
+        
+        
+        public int Execute<T>(string sql, object param)
+        {
+            return Dapper.SqlMapper.Execute(_conn, sql, param);
         }
         
 
