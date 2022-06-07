f233c691ef7f399d66cf1406fa8884a1f2edb18d
richie <richie.kwon1@gmail.com>
2022-06-07 21:16:18 +0100

    refactoring

--- BlueProject.Lib/Database/MySqlDapperHelper.cs
+++ BlueProject.Lib/Database/MySqlDapperHelper.cs
@@ -6,21 +6,22 @@ namespace BlueProject.Lib.Database
 {
     public class MySqlDapperHelper
     {
-        private MySqlConnection _conn;
+        private MySqlConnection _connection;
+        private MySqlTransaction _transaction;
         public MySqlDapperHelper()
         {
-            _conn = new MySqlConnection("Server=127.0.0.1;Database=myweb;Uid=root;Pwd=dookie91Sql!;");
+            _connection = new MySqlConnection("Server=127.0.0.1;Database=myweb;Uid=root;Pwd=dookie91Sql!;");
         }
 
         public List<T> Query<T>(string sql, object param)
         {
-            return Dapper.SqlMapper.Query<T>(_conn, sql, param).ToList();
+            return Dapper.SqlMapper.Query<T>(_connection, sql, param).ToList();
         }
+         
         
-        
-        public int Execute<T>(string sql, object param)
+        public int Execute(string sql, object param)
         {
-            return Dapper.SqlMapper.Execute(_conn, sql, param);
+            return Dapper.SqlMapper.Execute(_connection, sql, param);
         }
         
 
